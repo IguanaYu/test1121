6ad337879606c794e1da11dcffbf832f397aed94
iguana <iguana4623@qq.com>
2023-11-21 20:26:12 +0800

    oh !!!

--- functions/rules.py
+++
@@ -1,8 +0,0 @@
-def rangeRule(data, check=True, **kwargs):
-    rangeDict = kwargs.get('rangeDict')
-    enumerationInterval = rangeDict.get('EI', [])
-    rangeIntervals = rangeDict.get('RI', [])
-    if len(enumerationInterval) + len(rangeIntervals) == 0:
-        raise ValueError('未配置范围属性！')
-    
-    
--- projectDataQuality/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProjectdataqualityConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'projectDataQuality'
